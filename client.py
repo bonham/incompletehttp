7d81ee5f155c62e6a68e4ba5722a4b13c91e9b4b
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-06-08 22:15:48 +0200

    better server

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from pprint import pprint as pp
 
-with urlopen("http://localhost:13000") as r:
+with urlopen("http://localhost:9999") as r:
 #with urlopen("https://elfu.de") as r:
     print(r.url)
     print(r.code)
---
+++ socketserver2.py
@@ -0,0 +1,36 @@
+import socketserver
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+      # self.rfile is a file-like object created by the handler;
+      # we can now use e.g. readline() instead of raw recv() calls
+      self.data = self.rfile.readline().strip()
+      print("{} wrote:".format(self.client_address[0]))
+      print(self.data)
+      # Likewise, self.wfile is a file-like object used to write back
+      # to the client
+      #self.wfile.write(self.data.upper())
+
+      with open('response.txt', 'r+b') as f:
+          content = f.read()
+      print("----")
+      print(type(content))
+      print(len(content))
+      self.wfile.write(content)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()  
